2e59c9db25be8734badfc76aacad37568a131cfe
Sebastian Nanek <snanek@gmail.com>
2014-03-11 22:14:37 +0100

    Script for creating seed data in development

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,32 @@ cleared_classes.each do |klass|
   klass.delete_all
 end
 
-todos = [
+users = [
   {
-    content: Faker::Lorem.sentences,
-    due_date: Date.today + rand(12).days,
+    email: "snanek@gmail.com",
+    password: "123123123",
+    password_confirmation: "123123123"
+  },
+  5.times.collect do
+    {
+      email: Faker::Internet.email,
+      password: "123123123",
+      password_confirmation: "123123123"
+    }
+  end
+].flatten
+
+users.each { |u| User.create!(u) }
+
+user_ids = User.all.collect(&:id)
+
+
+todos = (20 + rand(60)).times.collect {
+  {
+    content: Faker::Lorem.sentences.join(" "),
+    due_date: Date.today + rand(22).days,
+    user_id: user_ids.sample
   }
-]
+}
+
 todos.each { |data| ToDo.new(data).save! }
