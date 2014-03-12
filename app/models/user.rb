688c57a2d57d97624caf6fc446366739910cac72
Sebastian Nanek <snanek@gmail.com>
2014-03-12 11:41:07 +0100

    User model: just formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-   begin :associations
-      has_many :to_dos
-   end
+  begin :associations
+    has_many :to_dos
+  end
 end
