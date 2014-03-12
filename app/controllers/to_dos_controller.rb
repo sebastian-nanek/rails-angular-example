d9fdfecf81e684171f99103d6e35542ad99ec1eb
Sebastian Nanek <snanek@gmail.com>
2014-03-12 11:40:23 +0100

    JSON request for GET /to_dos

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -1,8 +1,18 @@
 class ToDosController < InheritedResources::Base
   respond_to :js, :json
 
+  def index
+    super do |format|
+      format.json { render json: collection }
+    end
+  end
+
   private
   def collection
-    @to_dos ||= ToDo.all
+    @to_dos ||= end_of_association_chain.all
+  end
+
+  def begin_of_association_chain
+    current_user
   end
 end
--- spec/requests/to_dos_spec.rb
+++ spec/requests/to_dos_spec.rb
@@ -1,4 +1,40 @@
 require 'spec_helper'
 
 describe "GET /to_dos.json" do
+  let(:auth_token) { "" }
+
+  it "returns valid JSON" do
+    get to_dos_path(format: "json"), { :auth_token => auth_token }
+    expect { JSON.parse(response.body) }.not_to raise_exception
+  end
+
+  it "returns success response" do
+    get to_dos_path(format: "json"), { :auth_token => auth_token }
+
+    expect(response.status).to eq (200)
+  end
+
+  context "with a ToDo record stored in the DB" do
+    let(:sample_content) { "sample" }
+    let(:user)           { double(User, id: 42) }
+    let(:todo) do
+      ToDo.new({
+        content:   sample_content,
+        completed: false,
+        due_date:  Date.today,
+        user_id:   user.id
+      })
+    end
+
+    before do
+      todo.save!
+    end
+
+    it "returns serialized ToDos for current user" do
+      get to_dos_path(format: "json"), { :auth_token => auth_token }
+
+      todos = JSON.parse(response.body)["to_dos"]
+      expect(todos.first["content"]).to eq(sample_content)
+    end
+  end
 end
