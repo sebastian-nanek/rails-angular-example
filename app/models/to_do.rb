2476d2d270775f8b6cf078a2328b3a2cf968cc70
Sebastian Nanek <snanek@gmail.com>
2014-03-12 11:40:51 +0100

    Removing void validation from ToDo

--- app/models/to_do.rb
+++ app/models/to_do.rb
@@ -7,9 +7,6 @@ class ToDo < ActiveRecord::Base
     validates :priority,
       presence: true,
       numericality: { in: 1..5 }
-    validates :sort_order,
-      presence: true,
-      numericality: { greater_than_or_equal_to: 0 }
     validates :user_id,
       presence: true
   end
