398790d99cd5bf27aa116a45161c848b0fa3f06e
Sebastian Nanek <snanek@gmail.com>
2014-03-14 20:13:53 +0100

    Rescue from AR:RecordNotFound with status 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,15 @@ class ApplicationController < ActionController::Base
 
   before_filter :authenticate_by_token
 
+  rescue_from ActiveRecord::RecordNotFound do
+    case request.format
+    when "json"
+      render nothing: true, status: :not_found
+    when "html"
+      render "404.html"
+    end
+  end
+
   private
   def authenticate_by_token
     if params.has_key?(:auth_token) && token = AuthenticationToken.active.find_by_auth_token(params[:auth_token])
